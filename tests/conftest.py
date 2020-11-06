eb8f7d61f3908c421c37216787d9cf9c96f83a73
Eduardo Saporski <esaporski@protonmail.com>
2020-11-06 02:49:50 -0300

    creating environment with files and directories for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,28 @@
+import pathlib
 import pytest
 
 
-@pytest.fixture
-def example():
-    return True
+@pytest.fixture()
+def truenas_environment(tmpdir):
+    # Create '/etc', '/data' and '/data/geli' directories
+    etc = tmpdir.mkdir('etc')
+    data = tmpdir.mkdir('data')
+    geli = data.mkdir('geli')
+
+    # Create '/etc/version' file
+    version = etc.join('version')
+    version.write('FakeNAS-66.6-RELEASE (p314159265)')
+
+    # Create '/data/freenas-v1.db' file
+    db = data.join('freenas-v1.db')
+    db.write('fakenas')
+
+    # Create '/data/geli/*.key' file
+    key = geli.join('fake666ca86c9a1c460a17322666fake.key')
+    key.write('fakekey')
+
+    # Create '/data/pwenc_secret' file
+    secret = data.join('pwenc_secret')
+    secret.write('fakesecret')
+
+    return pathlib.Path(tmpdir)
