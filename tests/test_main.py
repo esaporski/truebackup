1a7a2c5282ed84daa46e3d3d0fc34e8fc044ea1d
Eduardo Saporski <esaporski@protonmail.com>
2020-11-06 22:22:46 -0300

    small refactor

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,33 +3,34 @@ import pytest
 from truebackup.main import compress, is_valid_path
 
 
-# is_valid_path
-@pytest.mark.parametrize(
-    'dir, expected', [
-        ('.', True),
-        ('..', True),
-        ('/tmp', True),
-        (pathlib.Path('/tmp'), True),
-        ('/invalid/path', False),
-        ('/root', False),
-        ('/etc/hostname', False),
-        ('\tmp', False),
-        (pathlib.Path('/invalid/path'), False)
-    ]
-)
-def test_is_valid_path(dir, expected):
-    assert is_valid_path(dir) is expected
+class TestIsValidPath:
+    # Test paths
+    @pytest.mark.parametrize(
+        'dir, expected', [
+            ('.', True),
+            ('..', True),
+            ('/tmp', True),
+            (pathlib.Path('/tmp'), True),
+            ('/invalid/path', False),
+            ('/root', False),
+            ('/etc/hostname', False),
+            ('\tmp', False),
+            (pathlib.Path('/invalid/path'), False)
+        ]
+    )
+    def test_is_valid_path(self, dir, expected):
+        assert is_valid_path(dir) is expected
 
-
-@pytest.mark.parametrize(
-    'dir', [
-        (1),
-        (None)
-    ]
-)
-def test_is_valid_path_error(dir):
-    with pytest.raises(TypeError):
-        is_valid_path(dir)
+    # Test unsupported argument types
+    @pytest.mark.parametrize(
+        'dir', [
+            (1),
+            (None)
+        ]
+    )
+    def test_is_valid_path_error(self, dir):
+        with pytest.raises(TypeError):
+            is_valid_path(dir)
 
 
 @pytest.mark.usefixtures('truenas_environment')
